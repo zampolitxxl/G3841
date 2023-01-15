5a2f748eefb26a746cad57fcf8786f0c92703365
Kirill Timofeev <sushi-maker@yandex.ru>
2023-01-15 20:26:46 +0300

    Seminar1Task1 working

--- Seminar1Task1/Program.cs
+++ Seminar1Task1/Program.cs
@@ -1,4 +1,19 @@
-﻿string? inputNum1 = Console.ReadLine();
+﻿Console.WriteLine("Введите первое число");
+string? inputNum1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
 string? inputNum2 = Console.ReadLine();
-if(inputNum1!=null inputNum1!=null) 
+if(inputNum1!=null && inputNum2!=null)
+{ 
+    int num1 = int.Parse(inputNum1);
+    int num2 = int.Parse(inputNum2);
 
+    if(num1==num2*num2)
+    {
+    Console.WriteLine("Первое число квадрат второго");
+
+    }
+    else
+    {
+    Console.WriteLine("Первое число не является квадратом второго");
+    }
+}
