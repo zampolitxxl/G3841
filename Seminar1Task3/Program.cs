b3adc1ee7ae200470b96fd701b7d40b55f907c57
Kirill Timofeev <sushi-maker@yandex.ru>
2023-01-16 12:48:45 +0300

    Seminar1Task3

--- Seminar1Task3/Program.cs
+++ Seminar1Task3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер, вам отобразится день недели");
+string? imputline = Console.ReadLine();
+if(imputline!=null)
+    {
+    string[] dayofWeek = new string[7];
+    string[0] = "Понедельник";
+    string[0] = "Вторник";
+    string[0] = "Среда";
+    string[0] = "Четверг";
+    string[0] = "Пятница";
+    string[0] = "Суббота";
+    string[0] = "Воскресенье";
+
+int imputnumber = int.Parse(imputline);
+
+    Console.WriteLine(dayweek[numberday]);
+
+    }
+string[] dayweek = new string[7];
+    string[0] = "Понедельник";
+    string[0] = "Вторник";
+    string[0] = "Среда";
+    string[0] = "Четверг";
+    string[0] = "Пятница";
+    string[0] = "Суббота";
+    string[0] = "Воскресенье";
