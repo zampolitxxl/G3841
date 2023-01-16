3a4e4fb6f34c4f452898aa593de1a065656b41f5
Kirill Timofeev <sushi-maker@yandex.ru>
2023-01-17 01:02:36 +0300

    Homework 1 done

---
+++ Seminar1Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+string? inputNum1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string? inputNum2 = Console.ReadLine();
+if(inputNum1!=null && inputNum2!=null)
+{ 
+    int num1 = int.Parse(inputNum1);
+    int num2 = int.Parse(inputNum2);
+
+    if(num1>num2)
+    {
+    Console.WriteLine("Первое число больше второго");
+
+    }
+    else
+    {
+    Console.WriteLine("Второе число больше первого");
+    }
+}
