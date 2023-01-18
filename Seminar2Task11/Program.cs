f34d02b6a496ab11c251c17eebe8e78f35cd16ff
Kirill Tim <sushi-maker@yandex.ru>
2023-01-18 20:27:35 +0300

    Task 11 , 12 for push

---
+++ Seminar2Task11/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Random numsintezator = new System.Random();
+int num = numsintezator.Next(100,1000);
+Console.WriteLine(num);
+int digit1 = num/100;
+int digit2 = num%10;
+Console.WriteLine(digit1 + "" + digit2);
+
---
+++ Seminar2Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1 = int.Parse(Console.ReadLine()??"0");
+int num2 = int.Parse(Console.ReadLine()??"0");
+int res = num2%num1;
+if(res==0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно");
+}
