c710045ba406dcbfce5441c40e14b8057a98959f
Kirill Tim <sushi-maker@yandex.ru>
2023-01-18 20:51:22 +0300

    Task 14 , 16 for push

---
+++ Seminar2Task14/Program.cs
@@ -0,0 +1,14 @@
+﻿int num1 = int.Parse(Console.ReadLine()??"0");
+int res1 = num1%7;
+int res2 = num1%23;
+if((res1==0)&&(res2==0))
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно "+ res1 +"" + res2);
+}
+
+
+
---
+++ Seminar2Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿int num1 = int.Parse(Console.ReadLine()??"0");
+int num2 = int.Parse(Console.ReadLine()??"0");
+if(num1*num1==num2)
+{
+    Console.WriteLine ("Второе число квадрат первого");
+    
+    ifelse (num2*num2==num1)
+    {
+            Console.WriteLine ("Первое число квадрат второго");
+
+    }
+}
+
+
