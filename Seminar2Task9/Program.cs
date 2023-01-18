f9ca636fc891f4301ad58906acdc1eec321d09b6
Kirill Tim <sushi-maker@yandex.ru>
2023-01-18 20:26:42 +0300

    Task 9

---
+++ Seminar2Task9/Program.cs
@@ -0,0 +1,35 @@
+﻿// System.Random numsintezator = new System.Random();
+
+// int rndNum = numsintezator.Next(10,100);
+
+// Console.Writeline ("случайное число" + rndNum);
+
+// int digit1 = rndNum/10;
+// int digit2 = rndNum%10;
+
+// if (digit1>digit2)
+// {
+//     Console.Writeline ("Цифра 1 больше цифры 2" + digit1 + "" + digit2);
+
+// }
+// else 
+// {
+//     Console.Writeline ("Цифра 2 больше цифры 1"  + digit1 + "" + digit2);
+
+// }
+
+
+//вариант 2
+
+// char[] digits = numsintezator.Next(10,100).ToString().ToCharArray();
+// int digit1 = (int)digits[0]-48;
+// int digit1 = (int)digits[0]-48;
+// int resNum = digit1>digit2?digit1:digit2;
+
+// Console.Writeline(resNum);
+
+//вариант 3
+
+int rndNum = numSintezator.Next()
+
+
