59c83820783bf2f0848b6f1d0c17653951bae78b
Kirill Timofeev <sushi-maker@yandex.ru>
2023-02-11 14:33:27 +0300

    Homework up to seminar 6 done

---
+++ Seminar5Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// ФУНКЦИЯ считываем данные стринг
+string ReadDataString(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine() ?? "0";
+}
+//ФУНЦИЯ Выводим результат
+void PrintData(string words, int info)
+{
+Console.Write(words);
+Console.WriteLine(info);
+}
+
+
+//Создаем метод куда вставлчем стринг и возвращаем инт кол-во полож. чисел
+
+int PosiNumCount(string text)
+{
+ 
+string[] words = text.Split(new char[] { ',' });
+
+//узнаем длину разбитого после СПЛИТ "массива"
+int len = words.Length;
+//создадим переменную подсчета положительных цифр
+int posiDigits=0;
+ //запустим цикл
+ for( int i = 0; i<len; i++)
+    
+    {int intWords = int.Parse(words[i]);//каждый раз конвертируем элемент ЧАР в ИНТ
+
+        if(intWords>0)
+        {
+        posiDigits=1+posiDigits;
+        }
+    
+    } 
+    return posiDigits;
+}
+
+//Выводим Рид Дата
+string textInput = ReadDataString("Введите пожалуйста числа для посчета");
+// запускаем методподсчета положительных
+int pos = PosiNumCount(textInput);
+//выводим рез-т
+PrintData("ответ:  ", pos);
+
---
+++ Seminar5Task43/Program.cs
@@ -0,0 +1,54 @@
+﻿// ФУНКЦИЯ считываем данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+//Функция вывода данных стринг + ИНТ
+void PrintData(string words, string info)
+{
+Console.Write(words);
+Console.WriteLine(info);
+}
+
+//Метод поиска координат точки пересечения двух прямых
+
+string Intersection(double b1, double k1, double b2, double k2)
+{
+    
+    double interPointX = (b2-b1)/(k1-k2);
+    double interPointY = k1*interPointX+b1;
+    
+
+    string Y=Convert.ToString(interPointY);
+    string X=Convert.ToString(interPointX);
+    string IntersectionPoint = "("+X+","+Y+")";
+    
+
+    return IntersectionPoint;
+}
+
+
+//Решение
+
+
+//Просим ввести поочередно координаты точек
+int b1 = ReadData("Введит координаты точки b1");
+int k1 = ReadData("Введит координаты точки k1");
+int b2 = ReadData("Введит координаты точки b2");
+int k2 = ReadData("Введит координаты точки k2");
+
+
+
+//Вычисляем точку перечения по методу
+//проверка на параллельность
+    if(k1==k2)
+    {
+    Console.WriteLine("Прямые не пересекаются");
+    }
+    else
+    {
+    string intPointRes = Intersection(b1, k1, b2, k2);
+    //Выводим ответ на экран
+    PrintData("Точка пересечения двух прямых имеет координаты: ", intPointRes);
+    }
